a3fadfd4caeee4931d6602bf22d66a98aa2d76e4
AleMonteros <alejandromonteros236@gmail.com>
2023-06-21 13:00:32 -0300

    new file:   aprendizaje-form.php 	new file:   aprendizaje-peticPyG.php 	new file:   recibir_get.php 	new file:   recibir_post.php

---
+++ recibir_post.php
@@ -0,0 +1,10 @@
+<?php
+	$usuario = $_POST['usuario'];
+	$password = $_POST['password'];
+
+	// Conectarse a la Base de Datos
+	// Autenticar al Usuario
+
+	echo "El usuario es ". $usuario . "<br>";
+	echo "El password es " . $password . "<br>";
+?>
