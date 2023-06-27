826071fd407fb601ed59243db70f199a80e774a1
AleMonteros <alejandromonteros236@gmail.com>
2023-06-27 12:54:08 -0300

    new file:   .vscode/settings.json 	new file:   aprendizaje-butt-form.php 	new file:   aprendizaje-check-form.php 	new file:   aprendizaje-fechas.php 	new file:   aprendizaje-funciones.php 	new file:   aprendizaje-list-form.php 	new file:   operaciones.php

---
+++ aprendizaje-butt-form.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Validar formulario</title>
+	<style>
+		body{
+            background-color: #264673; 
+            box-sizing: border-box; 
+            font-family: Arial;
+        }
+
+		form{
+			background-color: white;
+			padding: 10px;
+			margin: 100px auto;
+			width: 500px;
+		}
+
+		input[type=text], input[type=password]{
+			padding: 10px;
+			width: 480px;
+		}
+		input[type="submit"]{
+            text-align: center;
+			border: 0;
+			background-color: #ED8824;
+			padding: 10px 20px;
+            margin-top: 10px;
+		}
+
+		.error{
+			background-color: #FF9185;
+			font-size: 12px;
+			padding: 10px;
+		}
+		.correcto{
+			background-color: #A0DEA7;
+			font-size: 12px;
+			padding: 10px;
+		}
+	</style>
+</head>
+<body>
+	<form action="#" method="POST">
+		<?php
+
+            $nombre = "";
+            $password = "";
+            $email = "";
+            $pais = "";
+            $nivel = "";
+
+
+			if(isset($_POST['nombre'])){
+                $nombre = $_POST['nombre'];
+                $password = $_POST['password'];
+                $email = $_POST['email'];
+                $pais = $_POST['pais'];
+
+                if(isset($_POST['nivel'])){
+                    $nivel = $_POST['nivel'];
+                }else{
+                    $nivel = "";
+                }
+            
+                $campos = array();
+
+                if($nombre == ""){
+                    array_push($campos, "El campo Nombre no puede estar vaci.");
+                }
+
+                if($password == "" || strlen($password < 6)){
+                    array_push($campos, "El campo Contraseña no puede estar vacio, ni tener menos de 6 caracteres.");
+                }
+
+                if($email == "" || strpos($email, "@") === false){
+                    array_push($campos, "Ingresa un correo Electrónico valido.");
+                }
+
+                if($pais == ""){
+                    array_push($campos, "Selecciona un país de origen.");
+                }
+
+                if($nivel == ""){
+                    array_push($campos , "Selecciona un nivel de Desarrollo.");
+                }
+
+                if(count($campos) > 0){
+                    echo "<div class='error'>";
+                    for ($i = 0; $i < count($campos); $i++) {
+                        echo "<li>".$campos[$i]."</i>";
+                    }
+                } else{
+                    echo "<div class='correcto'> Datos Correctos";
+                }
+                echo "</div>";
+            }
+		?>
+		<p>
+		Nombre:<br/>
+		<input type="text" name="nombre" value="<?php echo $nombre; ?>">
+		</p>
+
+		<p>
+		Contraseña:<br/>
+		<input type="password" name="password" value="<?php echo $password; ?>">
+		</p>
+
+		<p>
+		Correo Electrónico:<br/>
+		<input type="text" name="email" value="<?php echo $email; ?>">
+		</p>
+
+        <p>
+            País de Origen : 
+            <select name="pais" id="">
+                <option value="">Selecciona un País</option>
+                <option value="arg" <?php if($pais == "arg") echo "selected"?>>Argentina</option>
+                <option value="uru" <?php if($pais == "uru") echo "selected"?>>Uruguay</option>
+                <option value="par" <?php if($pais == "par") echo "selected"?>>Paraguay</option>
+                <option value="chi" <?php if($pais == "chi") echo "selected"?>>Chile</option>
+                <option value="bol" <?php if($pais == "bol") echo "selected"?>>Bolivia</option>
+                <option value="bra" <?php if($pais == "bra") echo "selected"?>>Brasil</option>
+                <option value="per" <?php if($pais == "per") echo "selected"?>>Peru</option>
+            </select>
+        </p>
+
+        <p>
+            Nivel de Desarrollo :
+            <input type="radio" name="nivel" value="principiante" <?php if($nivel == "principiante") echo "checked" ?>> Principiante
+            <input type="radio" name="nivel" value="intermedio"  <?php if($nivel == "intermedio") echo "checked" ?>> Intermedio
+            <input type="radio" name="nivel" value="avanzado" <?php if($nivel == "avanzado") echo "checked" ?>> Avanzado
+        </p>
+
+		<p><input type="submit" value="Enviar Datos"></p> 
+	</form>
+</body>
+</html>
---
+++ aprendizaje-fechas.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Fechas</title>
+    <style>
+        body {
+            background-color: #5276af;
+            height: 100%;
+        }
+        #container {
+            background: white;
+            margin: 100px auto;
+            padding: 100px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    
+        <div id="container">
+        <?php 
+            /* 
+             --- d -- dia del mes (1-31)
+             --- m -- mes del año (1-12)
+             --- Y -- año (4 digitos)
+             --- l -- dia de la semana
+
+             --- h -- hora en formato 1-12
+             --- i -- minutos 0-59
+             --- s -- segundos 0-59
+             --- a -- pm / am
+            */
+            $mes = array("enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre");
+
+            echo "Fecha: ". date("l"). " ". date("d") . " de ". $mes[date("m")-1] . " del " . date("Y") . "<br>";
+            echo "Son las " . date("h:i:s-a");
+        ?>
+        </div>
+
+</body>
+</html>
